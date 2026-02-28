aca9a4f6ec8edbf8b6f07c8abca3aba8a31925ab
J4sU25 <juan.andres.salazar@correounivalle.edu.co>
2026-02-28 10:39:45 -0500

    ejercicio

--- ejercicio quiz.py
+++ ejercicio quiz.py
@@ -5,7 +5,7 @@ def imprimir_pares(arreglo, n):
             # Imprime la combinación de pares
             print(f"{arreglo[i]}-{arreglo[j]}")
 
-# generamos el arreglo y la variable espacio para que pueda imprimir nuestro arreglo 
+# generamos el arreglo y la variable espacio para que pueda imprimir las combinaciones de pares
 mi_arreglo = [1, 2, 3, 4, 5]
 espacio = len(mi_arreglo)
 imprimir_pares(mi_arreglo, espacio)
